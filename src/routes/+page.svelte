f8ace8c4e974200682494ad67ca5d1420823847b
chen_zhihu <tora_chen@outlook.com>
2025-09-30 23:12:52 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Navbar from "$lib/Navbar.svelte";
+  import Drawer from "$lib/Drawer.svelte";
+  import Footer from "$lib/Footer.svelte";
+</script>
+
+<!-- 导航栏 -->
+<Navbar />
+
+<!-- 左侧栏 -->
+<Drawer />
+
+<!-- Hero -->
+<div
+  class="hero min-h-screen"
+  style="
+        background-image: url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp);
+      "
+>
+  <div class="hero-overlay"></div>
+  <div class="hero-content text-neutral-content text-center">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+      <p class="mb-5">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+        excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+        id nisi.
+      </p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+
+<Footer />
