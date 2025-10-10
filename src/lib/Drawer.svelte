69afa5695d89dca27c9acf515204a2a2a82f0826
chen_zhihu <tora_chen@outlook.com>
2025-10-10 18:06:38 +0800

    refactor: 移除Typescript依赖与配置

--- src/lib/Drawer.svelte
+++ src/lib/Drawer.svelte
@@ -8,8 +8,8 @@
     ></label>
     <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
       <!-- Sidebar content here -->
-      <li><a>Sidebar Item 1</a></li>
-      <li><a>Sidebar Item 2</a></li>
+      <!-- <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li> -->
     </ul>
   </div>
 </div>
